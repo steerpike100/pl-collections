4d348df9628d166f0befa02f03fd194ef092c40f
SteveB <SteveB@MO01428.mfl.co.uk>
2019-07-17 09:41:34 +0100

    first commit

---
+++ src/main/java/Product.java
@@ -0,0 +1,26 @@
+public class Product {
+
+    private final String name;
+    private final int weight;
+
+    public Product(String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    public String toString(){
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+}
